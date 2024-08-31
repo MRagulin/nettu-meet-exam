cead48689362547bd19a54227764cf91b1ddec54
Mikhail <mikhail.ragulin@gmail.com>
2024-08-31 20:33:00 +0300

    Create dd.sh

---
+++ dd.sh
@@ -0,0 +1,4 @@
+curl -s --insecure -XPOST "https://s410-exam.cyber-ed.space:8083/api/v2/product_types/" \
+          -H "Content-Type: application/json" \
+          -H "Authorization: Token c5b50032ffd2e0aa02e2ff56ac23f0e350af75b4" \
+		      -d '{"name":"mragulin"}'
